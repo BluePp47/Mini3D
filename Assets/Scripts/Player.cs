b541dc555612ab60abe5141c50000c5e2b943ac8
피타쵸쵸 <djfb123@naver.com>
2025-05-19 10:33:52 +0900

    [fix] PlayerCS
    [fix] PlayerCS

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
 {
     public PlayerController Controller;
 
-    public void Awake()
+    private void Awake()
     {
         CharacterManager.Instance.Player = this;
         Controller = GetComponent<PlayerController>();
