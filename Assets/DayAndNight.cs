85f4147d444dcb107a4c766fd29f3322a85a3bc9
피타쵸쵸 <djfb123@naver.com>
2025-05-20 10:41:48 +0900

    Update DayAndNight.cs

--- Assets/DayAndNight.cs
+++ Assets/DayAndNight.cs
@@ -7,7 +7,7 @@ public class DayAndNight : MonoBehaviour
     [Range(0.0f, 1.0f)]
     public float time;
     public float fullDayLength;
-    public float startTime = 0.4f;
+    public float startTime = 0.2f;
     private float timeRate;
     public Vector3 noon;
 
