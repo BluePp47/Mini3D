fd31936c3a3c5644cab578d24339e24764146b74
피타쵸쵸 <djfb123@naver.com>
2025-05-21 15:35:32 +0900

    [feat]

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JumpPad : MonoBehaviour
+public class Jumppad : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float jumpForce = 15f;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            PlayerController player = other.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                player.jumpPower(jumpForce);
+            }
+        }
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -155,5 +156,6 @@ public class PlayerController : MonoBehaviour
             Gizmos.DrawRay(rays[i].origin, rays[i].direction * 0.7f);
         }
     }
-    
+
+ 
 }
